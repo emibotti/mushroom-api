c69070277e9e2ec8deac549e4ccde76728985d22
Felipe Facio Cabrera <felipefaciocabrera@192.168.1.28>
2023-07-11 18:40:15 -0300

    Small changes to organizations controller

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -1,20 +1,20 @@
 class OrganizationsController < ApplicationController
+  before_action :authenticate_user!
+
   def show
-    @organization = Organization.find(params[:id])
-    render json: @organization, status: :ok
+    organization = Organization.find(params[:id])
+    render json: organization, status: :ok
   end
 
   def create
-    @organization = Organization.new(name: params[:name])
-    if @organization.save
-      current_user.organization = @organization
-      if current_user.save
-        render json: @organization, status: :ok
-      else
-        render json: { message: "Couldnt assign org to user" }, status: :unprocessable_entity
-      end
+    organization = Organization.create!(name: params[:name])
+    current_user.organization = organization
+    if current_user.save
+      render json: organization, status: :ok
     else
-      render json: { message: "Couldnt save Org" }, status: :unprocessable_entity
+      render json: { message: "Couldnt assign org to user" }, status: :unprocessable_entity
     end
+  rescue ActiveRecord::RecordInvalid => invalid
+    render json: { message: invalid.record.errors.full_messages.first }, status: :unprocessable_entity
   end
 end
