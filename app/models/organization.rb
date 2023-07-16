42c0def301fdc835bf51f71c8a2d1c5f963dcd13
Emiliano Botti <emiliano@xmartlabs.com>
2023-07-16 17:00:43 -0300

    Update app/models/organization.rb

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -9,7 +9,6 @@ class Organization < ApplicationRecord
   end
 
   def invitation_code_expired?
-    !invitation_code_created_at.present? ||
-    invitation_code_created_at.present? && Time.now > invitation_code_created_at + ENV["EXPIRATION_TIME"].to_i.minutes
+    !invitation_code_created_at.present? || Time.now > invitation_code_created_at + ENV["EXPIRATION_TIME"].to_i.minutes
   end
 end
