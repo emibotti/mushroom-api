508c75a93f11c22b28a81552f05791705efbeafb
Emiliano Botti <emiliano@xmartlabs.com>
2023-08-06 17:11:38 -0300

    Adding custom language support from headers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,13 @@
 class ApplicationController < ActionController::API
-  before_action :set_current_organization
+  before_action :set_current_organization, :set_locale
 
   private
 
   def set_current_organization
     Organization.current_id = current_user.organization_id if user_signed_in?
+  end
+
+  def set_locale
+    I18n.locale = request.headers['Accept-Language'] || I18n.default_locale
   end
 end
